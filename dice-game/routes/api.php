f0475d0a8d468c2f6ec45844b2b6abf5ef6fd097
Oscar <oscanguera@gmail.com>
2024-10-03 01:24:41 +0200

    Realizado código login para usuarios

--- dice-game/routes/api.php
+++ dice-game/routes/api.php
@@ -8,3 +8,4 @@ use Illuminate\Support\Facades\Route;
     return $request->user();
 })->middleware('auth:sanctum');*/
 Route::post('/players', [UserController::class, 'register']);
+Route::post('/login', [UserController::class, 'login']);

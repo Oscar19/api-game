450849f320cce0847b17e044ece59e94e115fb0f
Oscar <oscanguera@gmail.com>
2024-10-03 00:15:08 +0200

    He introducido la ruta y el código de registro

--- dice-game/app/Http/Controllers/UserController.php
+++ dice-game/app/Http/Controllers/UserController.php
@@ -1,10 +1,34 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\User;
+
+use Illuminate\Support\Facades\Hash;
 
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function register(Request $request){
+        $request->validate(
+            [
+                'name' => ['nullable', 'string', 'max:100'],
+                'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+                'password' => ['required', 'string' ]
+            ]
+        );
+        $user = User::create(
+            [
+                'name' => $request->nickname,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ]
+        );
+        $token=$user->createToken('auth_token')->accessToken;
+
+        return response([
+            'token'=> $token,
+            'message'=>'Acabas de registrarte!!!',
+        ]);
+    }
 }
--- dice-game/routes/api.php
+++ dice-game/routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
+/*Route::get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+})->middleware('auth:sanctum');*/
+Route::post('/players', [UserController::class, 'register']);
